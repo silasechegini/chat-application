38c2fc938094a2bd64bdd575d0ae130855a720e6
Ngozi Echegini <silasngoziechegini@gmail.com>
2021-02-26 17:23:14 -0500

    basic chat application

---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,88 @@
+import { useState } from 'react';
+import MessageForm from './MessageForm';
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
+
+
+const ChatFeed = (props) => {
+
+    const {chats, activeChat, userName, messages} = props;
+
+    const chat = chats && chats[activeChat];
+
+    const [status, setStatus] = useState("");
+
+    const renderReadReceipts = (message, isMyMessage) => {
+        return chat.people.map((person, index) => person.last_read === message.id && (
+            <div 
+                key={`read_${index}`}
+                className="read-receipt"
+                style={{float: isMyMessage? 'right' : 'left', backgroundImage: `url(${person?.person?.avatar})`}}
+            />
+        ))
+    }
+
+    const renderMessages = () => {
+        const keys = Object.keys(messages);
+
+        return keys.map((key, index) => {
+            const message = messages[key];
+            const lastMessageKey = index === 0? null : keys[index - 1];
+            const isMyMessage = userName === message.sender.username;
+
+            return (
+                <div key={`msg_${index} style={{width: '100%'}}`}> 
+                    <div className="message-block">
+                        {
+                            isMyMessage 
+                            ? <MyMessage message={message}/>
+                            :<TheirMessage message={message} lastMessage={messages[lastMessageKey]}/>
+                        }
+                    </div>
+                    <div className="read-receipts" style={{marginRight: isMyMessage ? '18px' : '0', marginLeft: isMyMessage ? '0' : '68px'}}>
+                        {renderReadReceipts(message, isMyMessage)}
+                    </div>
+                </div>
+            )
+        })
+    }
+
+    renderMessages()
+
+    if(!chat) return 'Loading... '
+
+    const handleStatus = (event) => {
+        let user = props.userName;
+        console.log(`${user} is typing ...`);
+        setStatus(`${user} is typing ...`);
+    }
+
+    const handleKeyUp = (event) => {
+        setTimeout(() => {
+            setStatus("");
+        }, 10000);
+    }
+
+    window.addEventListener('keypress', handleStatus);
+    window.addEventListener('keyup', handleKeyUp);
+
+    return(
+        <div className="chat-feed">
+            <div className="chat-title-container">
+                <div className="chat-title">{chat?.title}</div>
+                <div className="chat-subtitle">
+                    {chat.people.map((person) => ` ${person.person.username}`)}
+                </div>
+            </div>
+            {renderMessages()}
+            <div style={{ height: '100px' }} />
+
+            <div className="message-form-container">
+                <p>{status}</p>
+                <MessageForm {...props} chatId={activeChat} onStatus={handleStatus} status={status}/>
+            </div>
+        </div>
+    )
+}
+
+export default ChatFeed;
